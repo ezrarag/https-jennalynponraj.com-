d25427d4cbc949d6782315d105ce88888bc4be5a
Ezra Haugabrooks <ezra@aymscores.com>
2025-10-22 14:04:48 -0500

    smooth scroll changes

--- src/components/SmoothScrollProvider.tsx
+++ src/components/SmoothScrollProvider.tsx
@@ -8,8 +8,7 @@ const SmoothScrollProvider = ({ children }: { children: React.ReactNode }) => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
+      gestureOrientation: 'vertical',
       smooth: true,
       mouseMultiplier: 1,
       smoothTouch: false,
